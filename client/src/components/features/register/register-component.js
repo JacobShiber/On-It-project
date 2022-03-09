89e365768cc9cd0efe50311b1058b325147575e6
ShimonSamay <shimonsamay7@gmail.com>
2022-03-09 18:14:19 +0200

    register added .

---
+++ client/src/components/features/register/register-component.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterButton from "../register-button/register-button";
+
+const Register = () => {
+  return (
+    <form>
+    <RegisterButton/>
+    </form>
+  );
+};
+
+export default Register;
