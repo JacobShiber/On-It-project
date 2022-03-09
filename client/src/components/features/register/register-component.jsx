199816da3b3b84306ba5fd389a3aec82b34d3be5
kerenhailu <keren4509@gmail.com>
2022-03-09 19:43:02 +0200

    login

--- client/src/components/features/register/register-component.jsx
+++ client/src/components/features/register/register-component.jsx
@@ -4,7 +4,7 @@ import React  from "react";
 const Register = () => {
   return (
     <form>
-    <RegisterButton/>
+    {/* <RegisterButton/> */}
     </form>
   );
 };
--- client/src/services/users/users.service.js
+++ client/src/services/users/users.service.js
@@ -1,18 +1,19 @@
 const basic_url = 'http://localhost:6500/users';
 
 
-const userLogin = async (userData) => {
+export const userLogin = async (userData) => {
     return await fetch(`${basic_url}/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(userData)
     })
-    .then(response => console.log(response))
+    .then(response => response.json())
+    .then(res => console.log(res))
     .catch(err => console.log(err));
 }
 
 
-const userRegister = async (userData) => {
+export const userRegister = async (userData) => {
     return await fetch(`${basic_url}/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
