a5e9560fb88de405ed61de989ecfec9f4ba2a5ba
kerenhailu <keren4509@gmail.com>
2022-03-10 14:32:51 +0200

    logIn

--- client/src/components/pages/login/loginPage-component.js
+++ client/src/components/pages/login/loginPage-component.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import logo from './Logo-text-white.png';
 import Login from '../../features/login/login-component';
-import Register from '../../features/register/register-component'
+import MainRegister from '../../features/register/main-register-component'
 // import Login from '../../features/login/login-component'
 
  const LoginPage=()=> {
@@ -10,16 +10,16 @@ import Register from '../../features/register/register-component'
   return (
     <div className='loginPage'>
       <section className='leftSize'>
-      <button onClick={() => {setAuthState('login')}}>Login</button>
-      <button onClick={() => {setAuthState('register')}}>Register</button>
       {
-        authState === 'login' ? <Login /> : <Register/>
+        authState === 'login' ? <Login /> : <MainRegister/>
       }
       </section> 
       <section className='rightSize'>
         <img src={logo} alt="logo"/>
-        <h3>Welcome to a place that will put you <br/>
-          in order for your studies</h3>
+        <h2>Welcome to a place that will put you <br/>
+          in order for your studies</h2>
+          <button onClick={() => {setAuthState('login')}}>Login</button>
+      <button onClick={() => {setAuthState('register')}}>Register</button>
       </section>
     </div>
   )
