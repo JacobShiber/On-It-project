e2a6dd8609dca413fd998d5d4f34a2c85bb0c786
kerenhailu <keren4509@gmail.com>
2022-03-10 13:55:54 +0200

    login

--- client/src/components/Routing/PageRouting.js
+++ client/src/components/Routing/PageRouting.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../features/login/login-component";
 import Register from "../features/register/register-component";
+import Home from "../pages/home/home-component";
 import LoginPage from "../pages/login/loginPage-component";
 
 const PageRouting=()=> {
@@ -12,6 +13,7 @@ const PageRouting=()=> {
             <Route exact path="/" element={<LoginPage />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/register" element={<Register />} />
+            <Route exact path="/home" element={<Home />} />
           </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/components/pages/home/home-component.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      Home
+      <h1>MEET THE TEAM</h1>
+      <section className="theTeam">
+        <article className="student">
+          <img src="" alt="picStudent" width="70px" height="70px" />
+          studentOne
+        </article>
+        <article className="student">
+          <img src="" alt="picStudent" width="70px" height="70px" />
+          studentOne
+        </article>
+        <article className="student">
+          <img src="" alt="picStudent" width="70px" height="70px" />
+          studentOne
+        </article>
+      </section>
+    </div>
+  );
+};
+export default Home;
