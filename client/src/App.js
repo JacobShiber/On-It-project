bc28b1640d4948154b96134e59e50a3c2dc97d50
Shay Belayneh <shay600195@gmail.com>
2022-03-22 15:38:43 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import PageRouting from './components/Routing/PageRouting';
-import {UsersContextProvider} from './context/users-context/users-context';
+import { UsersContextProvider } from './context/users-context/users-context';
 import GradesTable from "./components/pages/grades-table/grades-table"
 
 
@@ -9,13 +9,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <style>
-@import url('https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&family=Poppins:wght@500&display=swap');
-</style>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&family=Poppins:wght@500&display=swap');
+        </style>
         <UsersContextProvider>
-          <GradesTable/>
-          {/* <PageRouting/> */}
+          <GradesTable />
+          {/* <Footer /> */}
+          {/* <Navbar /> */}
+          <PageRouting/>
         </UsersContextProvider>
+
       </header>
     </div>
   );
--- client/src/components/Routing/PageRouting.js
+++ client/src/components/Routing/PageRouting.js
@@ -8,19 +8,21 @@ import LoginPage from "../pages/login/loginPage-component";
 import News from "../pages/news/news-component";
 import Schedule from "../pages/schedule/schedule-component";
 
-const PageRouting=()=> {
+const PageRouting = () => {
   return (
     <div>
-<BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<LoginPage />} />
-            <Route exact path="/login" element={<Login />} />
-            {/* <Route exact path="/register" element={<Register />} /> */}
-            <Route exact path="/home" element={<Home />} />
-            <Route exact path="/news" element={<News />} />
-            <Route exact path="/contact" element={<Contact />} />
-            <Route exact path="/schedule" element={<Schedule />} />
-          </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<LoginPage />} />
+          <Route exact path="/login" element={<Login />} />
+          {/* <Route exact path="/register" element={<Register />} /> */}
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/news" element={<News />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/schedule" element={<Schedule />} />
+          {/* <Route exact path="/footer" element={<Footer />} /> */}
+          {/* <Route exact path="/home" element={<Footer />} /> */}
+        </Routes>
       </BrowserRouter>
     </div>
   )
