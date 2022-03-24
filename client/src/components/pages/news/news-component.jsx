a934dcd71831f6f19e568fab3bd60b5403973777
JacobShiber <yaakovshiber@gmail.com>
2022-03-24 14:48:47 +0200

    styling the news page

--- client/src/components/pages/news/news-component.jsx
+++ client/src/components/pages/news/news-component.jsx
@@ -11,14 +11,14 @@ const News = () => {
 
   const user = JSON.parse(localStorage.getItem('user'));
   return (
-    <>
+    <div className="newsContainer">
     <h1 className='mainTitle'>New Updates</h1>
     <div className = "userCardInfo">
         <UserCardInfo />
       </div>
     <div>{user.isAdmin === true? <NewsAdmin /> : <NewsUser/>}</div>
     {/* // <button onClick = {}>Click</button> */}
-   </> 
+   </div> 
   )
 }
 export default News;
