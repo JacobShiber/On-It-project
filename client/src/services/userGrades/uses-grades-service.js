6e4eafd588b5be85ff43b341f5da9fe62b636102
ShimonSamay <shimonsamay7@gmail.com>
2022-03-30 15:08:41 +0300

    heruko deploy

--- client/src/services/userGrades/uses-grades-service.js
+++ client/src/services/userGrades/uses-grades-service.js
@@ -9,7 +9,6 @@ export const GetAllGrades = async () => {
         Authorization: `bearer ${localStorage.getItem("token")}`,
       },
     })
-    
       .then((res) => res.json())
       .catch((error) => console.log({ error: "the method get isnt work" }));
   } catch (error) {
